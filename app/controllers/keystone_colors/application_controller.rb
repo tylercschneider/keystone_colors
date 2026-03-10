29670912e8119aa55977458e33d75e1f9f39dc36
tylercschneider <tylercschneider@gmail.com>
2026-03-10 10:41:31 -0400

    Revert "Delegate missing route helpers to main_app for host layout links"
    This reverts commit a79dd4117436e1233464aa501e5c7471bf1e5319.

--- app/controllers/keystone_colors/application_controller.rb
+++ app/controllers/keystone_colors/application_controller.rb
@@ -1,8 +1,5 @@
-# frozen_string_literal: true
-
 module KeystoneColors
   class ApplicationController < ::ApplicationController
     helper KeystoneUiHelper
-    helper MainAppRoutesHelper
   end
 end
